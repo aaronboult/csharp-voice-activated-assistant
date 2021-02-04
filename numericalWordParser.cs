2a6e1f9c89f62caf9b015c972176b9f443a1573e
Aaron Boult <aaronboult25@gmail.com>
2021-02-04 20:46:58 +0000

    Fixed issues with numerical word parser regarding numbers read by individual digits

--- numericalWordParser.cs
+++ numericalWordParser.cs
@@ -75,6 +75,14 @@ namespace MathsAbstractions{
 
             __PerformTest__("point zero zero zero seven", 0.0007);
 
+            __PerformTest__("one hundred and ninety thousand", 190_000);
+
+            __PerformTest__("five hundred thirty", 530);
+
+            __PerformTest__("two million eighty four thousand", 2_084_000);
+
+            __PerformTest__("five eight six", 586);
+
         }
 
         private static void __PerformTest__(string phrase, double expected, bool debug = false){
@@ -89,6 +97,8 @@ namespace MathsAbstractions{
 
             }
 
+            // 0.00000000000000001 is an arbitrary value; a small value is required to be able to differentiate
+            // incorrect calculations from floating point error calculations
             if (difference != 0 && difference < 0.00000000000000001){
 
                 Console.WriteLine("Phrase: {phrase}, Passed: True (floating point inaccuracy)");
@@ -109,17 +119,19 @@ namespace MathsAbstractions{
 
             double accumulator = 0;
 
-            bool chainedPlaceValue = false;
+            bool chainedPlaceValue = false; // A chained place value is something like "hundred thousand"
 
             bool previousWasTens = false;
 
             bool multiPlaceValue = false; // Whether a number is constructed using multiple places, i.e. two hundred and ten thousand
 
-            bool nextIsDecimal = false;
+            bool nextIsDecimal = false; // Decimal refers to a value after a decimal point, i.e. .1
 
             (int count, bool on) decimalToggle = (0, false);
 
             string[] words = phrase.Split(' ');
+
+            // Iterate over each component in the number and determine whether its a number, place or otherwise
 
             for (int i = 0 ; i < words.Length ; i++){
 
@@ -139,19 +151,28 @@ namespace MathsAbstractions{
 
                     (double value, WordGroups group) nextMatch = GetMappedWordValue(words[i + 1]);
 
-                    chainedPlaceValue = match.group == WordGroups.PLACE && nextMatch.group == WordGroups.PLACE;
+                    chainedPlaceValue = match.group == WordGroups.PLACE && nextMatch.group == WordGroups.PLACE; // If next word is a place value, it is a chained place value
 
                     nextIsDecimal = nextMatch.group == WordGroups.COMMANDS && nextMatch.value == 0; // nextMatch.value is 0 when indicating a decimal has started
 
+                    // The number could be two hundred and five thousand, therefore the 'and' needs to be considered in the offset
                     nextPlaceCheckOffset = nextMatch.group == WordGroups.CONNECTOR ? 3 : 2;
 
-                    if (i + nextPlaceCheckOffset - 1 < words.Length){
+                    if (i + nextPlaceCheckOffset < words.Length){
 
-                        (double value, WordGroups group) upcomingMatch = GetMappedWordValue(words[i + nextPlaceCheckOffset - 1]);
+                        // The upcoming match is the word match one place before where the next place value is expected
+                        (double value, WordGroups group) upcomingMatch = GetMappedWordValue(words[i + nextPlaceCheckOffset]);
 
-                        if (upcomingMatch.group == WordGroups.TENS){
+                        if (upcomingMatch.group != WordGroups.PLACE){
+                            
+                            upcomingMatch = GetMappedWordValue(words[i + nextPlaceCheckOffset - 1]);
 
-                            nextPlaceCheckOffset++;
+                            if (upcomingMatch.group == WordGroups.TENS){
+
+                                // Increase the offset by 1 if the next number contains both a ten and another number, i.e. twenty five
+                                nextPlaceCheckOffset++;
+
+                            }
 
                         }
 
@@ -177,6 +198,8 @@ namespace MathsAbstractions{
 
                     }
 
+                    // If the next number is succeeded by a place value, and the next place value is of a lesser order,
+                    // consider the two values to be connected. E.g. five million and seventy thousand
                     multiPlaceValue = upcomingMatch.group == WordGroups.PLACE && (upcomingMatch.value > match.value);
 
                 }
@@ -193,6 +216,7 @@ namespace MathsAbstractions{
                         continue;
 
                     case WordGroups.COMMANDS:
+                        // Determine which command is being used
                         switch (match.value){
                             case 0:
                                 decimalToggle = (1, true);
@@ -202,7 +226,7 @@ namespace MathsAbstractions{
 
                     case WordGroups.PLACE:
                         if (place.set && !multiPlaceValue){
-                            place.value *= match.value;
+                            place.value *= match.value; // Used with adjacent place values, e.g. hundred thousand
                         }
                         else{
                             place = (match.value, true);
@@ -213,11 +237,14 @@ namespace MathsAbstractions{
 
                     case WordGroups.TENS:
                         if (decimalToggle.on){
+                            // Append the new decimal digit by using powers of 0.1 to move down the place-values
                             number.value += match.value * Math.Pow(0.1, decimalToggle.count + 1);
                             number.set = true;
                             decimalToggle.count++;
                         }
                         else if (multiPlaceValue){
+                            // Move the already-assigned value up the place values before appending the new number
+                            // Stops "two million two hundred" losing the "two millions"s significance
                             number = ((number.value * place.value) + match.value, true);
                             place = (0, false);
                         }
@@ -229,20 +256,26 @@ namespace MathsAbstractions{
 
                     case WordGroups.NUMBER:
                         if (decimalToggle.on){
+                            // If a place value is assigned, move the value into its correct place-value position
+                            // Before appending decimals in order to maintain their significance
                             if (place.set){
                                 number.value *= place.value;
                                 place = (0, false);
                             }
+                            // Append the new decimal digit by using powers of 0.1 to move down the place-values
                             number.value += match.value * Math.Pow(0.1, decimalToggle.count + (int)(match.value / 10));
                             number.set = true;
                             decimalToggle.count++;
                         }
                         else if (multiPlaceValue){
+                            // Shift the already-set value into its correct place-value position before appending the new value
                             number = ((number.value * (place.set ? place.value : 1)) + match.value, true);
                         }
                         else if (number.set && !previousWasTens){
+                            // Allows for saying consecutive digits, such as "one four three"
                             accumulator += number.value;
                             accumulator *= 10;
+                            number.value = match.value;
                         }
                         else{
                             number = (match.value + number.value, true);
@@ -260,6 +293,8 @@ namespace MathsAbstractions{
                     Console.WriteLine($"\n\n\n\nNumber: {number}\nPlace: {place}\nAccumulator: {accumulator}\n\n\n\n");
 
                 }
+
+                // Separated conditions to avoid having an absurdly long if(...)
 
                 bool lastIteration = i + 1 == words.Length;
 
